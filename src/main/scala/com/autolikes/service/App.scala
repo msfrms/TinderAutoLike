59a8c9f82cf0513e2250431648a68c3292728925
Radaev Mikhail <msfrms@gmail.com>
2021-06-13 16:19:20 +0300

    add handle error code: 429

--- src/main/scala/com/autolikes/service/App.scala
+++ src/main/scala/com/autolikes/service/App.scala
@@ -1,31 +1,32 @@
 package com.autolikes.service
 
-import com.autolikes.service.core.RepeatFilter
+import com.autolikes.service.core.{RandomDelayFilter, RepeatFilter}
 import com.autolikes.service.like.LikeService
 import com.autolikes.service.recommendation.RecommendationService
 
-import com.twitter.finagle.Service
-import com.twitter.finagle.util.DefaultTimer
-import com.twitter.util.{Await, Future, Timer}
 import com.twitter.conversions.DurationOps.RichDuration
+import com.twitter.finagle.Service
+import com.twitter.util.{Await, Future}
 
 object App {
 
   def main(args: Array[String]): Unit = {
 
-    implicit val timer: Timer = DefaultTimer
-
     val mainService = Service.mk[Unit, Unit](_ =>
       for {
         userIds <- RecommendationService().apply(())
         _ <- Future
-          .traverseSequentially(userIds)(LikeService.byId)
-          .delayed(5.seconds)
+          .traverseSequentially(userIds)(id =>
+            RandomDelayFilter(from = 5.seconds, to = 15.seconds)
+              .andThen(LikeService.byId)
+              .apply(id)
+          )
           .onSuccess(println)
       } yield ()
     )
 
-    val appFuture = RepeatFilter().andThen(mainService).apply(())
+    val appFuture =
+      RepeatFilter(delay = 1.minute).andThen(mainService).apply(())
 
     Await.ready(appFuture)
   }
